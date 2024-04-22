c9bb253efbfd00eea7aa5bc7963f4395924001c9
BlackAntik <153559633+BlackAntik@users.noreply.github.com>
2024-04-22 10:01:37 +0300

    Update fifth.py

--- fifth.py
+++ fifth.py
@@ -5,9 +5,8 @@ from sklearn.metrics import mean_absolute_error as mae
 def get_domain(s):
     global nnm, sit_num
     num = int(re.findall(r'\d+', s)[0])
-    if nnm.get(num) is None:
-        nnm[num] = sit_num
-        sit_num += 1
+    #if nnm.get(num) is None:
+    #    nnm[num] = 0
     return nnm[num]
 
 
@@ -15,21 +14,39 @@ def get_path(s):
     nums = re.findall(r'\d+', s)
     return int(nums[1])
 
+#def bad_index(row):
+#    return row['user_id'] not in ids_set or len(row['referer']) == 0
+
 def bad_index(row):
-    return row['user_id'] not in ids_set or len(row['referer']) == 0
+    return len(re.findall(r'\d+', row['referer'])) != 2 or nnm.get(int(re.findall(r'\d+', row['referer'])[0])) is None or row['user_id'] not in ids_set
 
 
 new_users = pd.read_csv('data/test_users.csv')
-nreq = pd.read_parquet('../data/requests/part_0.parquet')
 nnm = pd.read_csv('data/nnm.csv')
+print(len(nnm))
 site_clust = pd.read_csv('data/site_clust2.csv')
 av_gen = pd.read_csv('data/av_gen2.csv')
 av_age = pd.read_csv('data/av_age2.csv')
 sit_num = len(site_clust)
 
 
-ids_set = set(new_users['user_id'])
+ids_set = set(new_users['user_id']),
 new_users = new_users.set_index('user_id')
+nreq = pd.read_parquet('../data/requests/part_0.parquet')
+
+
+bad_indexes = nreq[nreq.apply(bad_index, axis=1)].index
+nreq = nreq.drop(bad_indexes, axis=0)
+nreq = nreq.reset_index()
+    
+nreq = nreq.drop('index', axis=1)
+nreq['domain_id'] = nreq['referer'].apply(get_domain)
+nreq['path'] = nreq['referer'].apply(get_path)
+nreq['age'] = list(new_users['age'].loc[list(nreq['user_id'])])
+nreq['gender'] = list(new_users['gender'].loc[list(nreq['user_id'])])
+del nreq['referer']
+del nreq['user_agent']
+
 
 #for i in range(20, 30):
 for i in range(30):
@@ -37,6 +54,7 @@ for i in range(30):
         continue
     req = pd.read_parquet('../data/requests/part_'+str(i)+'.parquet')
     bad_indexes = req[req.apply(bad_index, axis=1)].index
+    req = req.drop(bad_indexes, axis=0)
     req = req.reset_index()
     
     req = req.drop('index', axis=1)
@@ -46,17 +64,27 @@ for i in range(30):
     req['gender'] = list(new_users['gender'].loc[list(req['user_id'])])
     del req['referer']
     del req['user_agent']
-
-    req = req.drop(bad_indexes, axis=0)
-
     
     nreq = pd.concat([nreq, req])
 
+'''
+bad_indexes = nreq[nreq.apply(bad_index, axis=1)].index
+nreq = nreq.drop(bad_indexes, axis=0)
+nreq = nreq.reset_index()
+    
+nreq = nreq.drop('index', axis=1)
+nreq['domain_id'] = nreq['referer'].apply(get_domain)
+nreq['path'] = nreq['referer'].apply(get_path)
+nreq['age'] = list(new_users['age'].loc[list(nreq['user_id'])])
+nreq['gender'] = list(new_users['gender'].loc[list(nreq['user_id'])])
+del nreq['referer']
+del nreq['user_agent']
+'''
+if len(nreq) == 0:
+    print("FLEX")
 
 #pepdist_clust, pep_clust, av_gen, av_age, avpep_gen, avpep_age, visit
-US_NUM = 20000000
 freq = [0] * sit_num
-sos_clust = [0] * US_NUM
 nusers = nreq.groupby("user_id")
 our_ans_age = []
 our_ans_gen = []
@@ -75,8 +103,8 @@ for ky in nusers:
             ind = us[i]
     our_ans_age.append(av_age[site_clust[ind]])
     our_ans_gen.append(av_age[site_clust[ind]])
-    right_ans_age.append(ky[1]["age"][0])
-    right_ans_gen.append(ky[1]["gender"][0])
+    right_ans_age.append(list(ky[1]["age"])[0])
+    right_ans_gen.append(list(ky[1]["gender"])[0])
 
 print('age mean absolute error', mae(our_ans_age, right_ans_age))
 print('gen mean absolute error', mae(our_ans_gen, right_ans_gen))
